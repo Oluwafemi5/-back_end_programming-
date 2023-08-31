c844b573124cf10ab385abef3b282a5baf39f14a
James Okonita <118991124+Oluwafemi5@users.noreply.github.com>
2023-08-31 23:25:59 +0100

    3-string_methods.py

---
+++ 3-string_methods.py
@@ -0,0 +1,10 @@
+myString = "Python is a cool program, Python is love, Python if fun"
+
+Counting= myString.count("Python")
+print(" 'Python' was counted :", Counting)
+
+cap = myString.upper()
+print(cap)
+
+cap_python = myString.replace("Python", "PYTHON")
+print(cap_python)
