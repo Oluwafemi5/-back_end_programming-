7b99bca5163c4d90d07e9f42a38cfaa22f928a98
James Okonita <118991124+Oluwafemi5@users.noreply.github.com>
2023-09-13 22:11:12 +0100

    2-fun_out.py

---
+++ pyFunctions/2-fun_out.py
@@ -0,0 +1,22 @@
+from random import randint
+
+Alpha= randint(1, 10)
+
+def play_game():
+    attempts = 3  
+  
+    while attempts > 0:
+        userNumber = int(input('Guess the Number: '))
+        
+        if userNumber == Alpha:
+            print("Congratulations! You guessed the number correctly!")
+            break
+        elif attempts == 1:
+            print(f"Sorry, you've run out of attempts. The correct number was {Alpha}.")
+            break
+        else:
+            print("Sorry, that's not the correct number. Try again.")
+        
+        attempts -= 1
+   
+play_game()
